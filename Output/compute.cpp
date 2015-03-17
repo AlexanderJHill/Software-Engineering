ddd3a9bdd51e11c9403c191b097ad8bf68d1b447
Orielle Yu <oby1@scarletmail.rutgers.edu>
2015-03-17 19:14:36 -0400

    Create compute.cpp

---
+++ Output/compute.cpp
@@ -0,0 +1,30 @@
+#include "compute.h"
+#include <iostream>
+Compute::Compute(){
+    
+}
+
+void Compute::calcOutput(){
+    //fisherman vs time of day
+    //percentage of success vs # fisherman, weather,experience and skill
+}
+
+void Compute::resultGen(){
+    //real time simulation
+    
+}
+
+void Compute::dataUpdate(){
+    //experience
+    //skill
+    //strategy
+}
+
+int Compute::stratGuide(){
+
+    return strategy;
+}
+
+Compute::~Compute(){
+    
+}
