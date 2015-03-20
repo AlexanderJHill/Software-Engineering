0d1fd7766a14e5010dfa1930156bd98a5381c25d
Orielle Yu <oby1@scarletmail.rutgers.edu>
2015-03-20 11:57:23 -0400

    Create results.h

---
+++ Output/results.h
@@ -0,0 +1,21 @@
+#ifndef ____results__
+#define ____results__
+
+#include <stdio.h>
+
+class Results{
+private:
+    float capital;
+    float bestStratIndex;
+    float bestStratScore;
+    float avgStratScore;
+public:
+    Results();
+    float computeAgentResults(float results,float decision);
+    float getAgentResults();
+    
+    
+};
+
+
+#endif /* defined(____results__) */
