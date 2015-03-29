cd37e0a586acade5a38292ecc29cd496e23de2e3
Orielle Yu <oby1@scarletmail.rutgers.edu>
2015-03-28 22:17:46 -0400

    Update spot.h
    Added function descriptions

--- Spot/spot.h
+++ Spot/spot.h
@@ -1,3 +1,4 @@
+//Used to create a spot and calculate how crowded a spot is.
 #ifndef ____spot__
 #define ____spot__
 
@@ -6,15 +7,43 @@ class Spot{
 private:
     double maxcapacity;//max agents per spot
     int numAgent;//number of agents possibly going fishing per spot
-
-
 public:
-    Spot();//initialize
-    void setCap(double cap);//set the maxcapacity per spot
-    double getSpotCapacity();//get back maxcapacity
-    void setAgentNum(int fisherNum);//total number of agents possibly going to spot
-    int getAgentNum();//get back total deciding agents
-    double crowdness(double goFish);//calculate the crowdness
+    /*Constructor for a spot
+    @pre none
+    @post numAgent and maxcapacity is initialized to zero
+    @return none
+    */
+    Spot();
+    /*Set the maxcapcity of agents for a given spot
+    @pre A spot is initialized and the number for capacity(cap) is given.
+    @post The maxcapacity is set equal to the given cap
+    @return none
+    */
+    void setCap(double cap);
+    /*Get the maxcapacity 
+    @pre A capacity is already set
+    @post none
+    @return The number of maxcapacity 
+    */
+    double getSpotCapacity();
+    /*Set the number of agents possibly going to spot
+    @pre Give the number of agents called fisherNum
+    @post The numAgent is equal to given fisherNum
+    @return none
+    */
+    void setAgentNum(int fisherNum);//
+    /*Get back total number agents
+    @pre Number of agents is already set
+    @post none
+    @return Integer number of total agents (numAgent)
+    */
+    int getAgentNum();
+    /*Calculate how crowded a spot is
+    @pre Give number of agents that decided to go fishing
+    @post Percentage of crowd is calculated
+    @return Percentage of fisherman going fishing
+    */
+    double crowdness(double goFish);
 };
 
 #endif /* defined(____spot__) */
