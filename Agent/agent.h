26252dcb480b4aaaeecc439a46dac52b7cdcb22a
Alexander Hill <ahill6984@gmail.com>
2015-03-29 17:57:11 -0400

    Modified some of the comments in the Agent and strategy classes to follow Doxygen formatting.

--- Agent/agent.h
+++ Agent/agent.h
@@ -4,47 +4,67 @@
 #include "strategy.h"
 #include <list>
 #include <vector>
+
+//!
+//! \brief Simulates the actions of an individual Fisher  
+//!  
+//!
+
+
 using namespace std;
 
-static int numOfAgent;
+static int numOfAgent;  /*! records the total number of agents created. */
 
+//! influence threshold, if based on report > 70 will make agent's decison change to 1
+//! < 70 agent's decision remain the same
+//! new rule: p => 85 change decision to 1
+//!			 40 < p < 85 decision remain
+//!			 p <= 40 decision change to -1
 class Agent{
 private:
-	vector<Strategy *> strat; //holds the 3 unique strategies
-	vector<int> history; //holds the last 3 outcomes
-	int decision; //new decision *have to calculate, use makeDecision
-	int earlydecison; //based on history sequence and strategy
-	double skill; // will be 1-5 *from input
-	float fishduration; //*from input
-	float temp; //*from input
+	vector<Strategy *> strat; 	//! holds the 3 unique strategies
+	vector<int> history; 		//! holds the last 3 outcomes
+	int decision; 				//! new decision *have to calculate, use makeDecision
+	int earlydecison; 			//! based on history sequence and strategy
+	double skill; 				//! will be 1-5 *from input
+	float fishduration; 		//! *from input
+	float temp; 				//! *from input
 	//int typefish; //number of fish *from input
-	int communication; //*from input
-	//influence threshold, if based on report > 70 will make agent's decison change to 1
-	// < 70 agent's decision remain the same
-	// new rule: p => 85 change decision to 1
-	//			 40 < p < 85 decision remain
-	//			 p <= 40 decision change to -1
+	int communication; 			//! *from input
+	
+	//! influence threshold, if based on report > 70 will make agent's decison change to 1
+	//! < 70 agent's decision remain the same
+	//! new rule: p => 85 change decision to 1
+	//!			 40 < p < 85 decision remain
+	//!			 p <= 40 decision change to -1
 	float threshold; //calculate using calcThreshold
 
 
 public:
-	Agent(vector<Strategy *> strat); //constructor
+	Agent(vector<Strategy *> strat); 	//! constructor
 	void updateStrategyScore(int winnigScore);
 	void calcThreshold();
 	void makeEarlyDecision();
-	void makeDecision(); //will be based on earlydecision and threshold
-	void updateHistory(); //push new decision
+	void makeDecision(); 				//! will be based on earlydecision and threshold
+	void updateHistory(); 				//! push new decision on
 
-	void setTemp(float newTemp); //from input
-	void setSkill(int newskill); //can be randomize
-	void setFishduration(float newFishDuration); //can be randomize
+	void setTemp(float newTemp); 		//! from input
+	void setSkill(int newskill); 		//! can be randomize
+	void setFishduration(float newFishDuration); //! can be randomize
 	void setCommunication(int newCommunication);
 
 	vector<int> getHistory();
+	
+	//! Returns the Decision of the Agent
 	int getDecision();
+	
+	//! Returns the amount the agent communicates with other agents. 
 	int getCommunication();
+	
+	//! Returns the current skill of the agent 
 	int getSkill();
 	//int getTypeFish();
+	
 	float getTemp();
 	float getFishDuration();
 	int getEarlyDecision();
--- Agent/strategy.h
+++ Agent/strategy.h
@@ -6,14 +6,14 @@
 #include <list>
 using namespace std;
 
+//! since each startegy depends on 3 previous outcomes, so posiible output
+//! for one strategy is 8.
+//! the sequence for the 3 previous outcomes would be:
+//! 000,001,010,...,111
+//! special case for starategy: 0->stay at home, 1->go fishing
 class Strategy{
 private:
     int score; //hold score for strategy
-	//since each startegy depends on 3 previous outcomes, so posiible output
-	//for one strategy is 8.
-	//the sequence for the 3 previous outcomes would be:
-	//000,001,010,...,111
-	//special case for starategy: 0->stay at home, 1->go fishing
 	vector<int> decisionPattern;
 public:
     Strategy(vector<int> randDecision);
