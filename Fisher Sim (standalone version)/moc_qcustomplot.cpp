b26d2a19bd07241563f5b22bfabc108d2fdc42f0
David Lazaar <navelops@gmail.com>
2015-05-01 01:13:04 -0400

    no message

--- Fisher Sim (standalone version)/moc_qcustomplot.cpp
+++ Fisher Sim (standalone version)/moc_qcustomplot.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../Documents/Software-Engineering/CrowdAnalysys/qcustomplot.h"
+#include "../../../Documents/Software-Engineering/CrowdAnalysys/qcustomplot.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #include <QtCore/QList>
