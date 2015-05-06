c984e827e24f3c92c7d7dbe184c38b07197dfc3d
Orielle Yu <oby1@scarletmail.rutgers.edu>
2015-05-06 12:12:49 -0400

    Update spot.cpp

--- Spot/spot.cpp
+++ Spot/spot.cpp
@@ -1,58 +1,69 @@
-//Created and edited by Orielle Joy Yu
-#include <iostream>
+//! \file spot.h 
+//! \brief Used to create a spot and calculate how crowded a spot is.
+
+//! \brief Created and edited by Orielle Joy Yu
+#ifndef ____spot__
+#define ____spot__
+
+#include <stdio.h>
 #include <list>
-#include "../Agent/agent.h"
-#include "../Agent/strategy.h"
-#include "spot.h"
-/*
- constructor
- */
-Spot::Spot(list<Agent *> newAgents){
-    agents = newAgents;
-    numAgent = 0;//Initial
-    maxcapacity = 0;
-}
+#include "agent.h"
 
-/*
- maxcapacity of fisherman variable is initialized for a given spot
- */
-void Spot::setCap(double cap) {
-    maxcapacity = cap;
-}
+//! \brief Spot is used to create a location and calculate how crowded it is.
+class Spot{
+private:
+    double maxcapacity; 	//!< max agents per spot
+    int numAgent;		//!< number of agents possibly going fishing per spo
+    list<Agent *> agents;     //!< list of agents
+public:
+    
+	/*! Constructor for a spot
+    @pre A list of agents
+    @post numAgent and maxcapacity is initialized to zero, sets a new list of agents
+    @return none
+    */
+    Spot(list<Agent *> newAgents);
+    
+	/*Set the maxcapcity of agents for a given spot
+    @pre A spot is initialized and the number for capacity(cap) is given.
+    @post The maxcapacity is set equal to the given cap
+    @return none
+    */
+    void setCap(double cap);
+    
+	/*! Get the maxcapacity 
+    @pre A capacity is already set
+    @post none
+    @return The number of maxcapacity 
+    */
+    double getSpotCapacity();
+    
+	/*! Set the number of agents possibly going to spot
+    @pre Give the number of agents called fisherNum
+    @post The numAgent is equal to given fisherNum
+    @return none
+    */
+    void setAgentNum(int fisherNum);//
+    
+	/*! Get back total number agents
+    @pre Number of agents is already set
+    @post none
+    @return Integer number of total agents (numAgent)
+    */
+    int getAgentNum();
+    /*! 
+    @pre A Spot wa initialized
+    @post Creates a list of agents
+    @return List of agents
+    */
+    list<Agent *> getAgents();
+};
 
-/* 
- Returns a spot's maxcapacity
- */
-double Spot::getSpotCapacity() {
-    return maxcapacity;
-}
+#endif /* defined(____spot__) */
 
-/*
- set the number of agents going, from input
- */
-void Spot::setAgentNum(int fisherNum){
-    numAgent = fisherNum;
-}
-
-/*
- Returns number of Agents on spot
- */
-int Spot::getAgentNum() {
-    return numAgent;
-}
-/*
-Returns a list of agents
+/*! Calculate how crowded a spot is
+    @pre Give number of agents that decided to go fishing
+    @post Percentage of crowd is calculated
+    @return Percentage of fisherman going fishing
 */
-
-list<Agent *> Spot::getAgents(){
-    return agents;
-}
-
-/* @pre:
- Computes the crowdness, in percentage
- */
-double crowdness(double goFish, double maxCap) {
-    double crowdness =(goFish/maxCap)*100;
-    //cout<<crowdness<<'\n';
-    return crowdness;
-}
+double crowdness(double goFish, double maxCap);
