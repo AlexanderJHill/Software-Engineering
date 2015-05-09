fa08a5e0643d793ac047c22bb1a9b0ee56ccd81f
Alexander Hill <ahill6984@gmail.com>
2015-05-09 15:36:40 -0400

    Added more files to the Archive docs and rebuilt  the technical documentation.

--- CrowdAnalysys/drawing.h
+++ CrowdAnalysys/drawing.h
@@ -1,12 +1,20 @@
 #ifndef DRAWING_H
 #define DRAWING_H
 
+//! \file drawing.h
+//! \brief Provides a method of drawing locations and the number of people in each location.
+
 #include <QMainWindow>
 #include <QtWidgets>
 #include <QVector>
 #include "dlocation.h"
 
-
+//! \brief Provides a method of drawing locations and the number of people in each location.
+//!
+//! Drawing is used by the main window to create the real time display. It provides methods
+//! to get and set the number of locations and the number of people at each location. Setting
+//! different values is not updated on the screen without calling ReDraw.
+//!
 class Drawing
 {
 public:
